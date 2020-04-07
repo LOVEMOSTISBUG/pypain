a643b15233b70bb919e6e249b67aa68750c501f8
LOVEMOSTISBUG <56868054+LOVEMOSTISBUG@users.noreply.github.com>
2020-04-08 00:01:24 +0800

    本次更新了学习说话 总之变得聪明一点了

--- CoolQ 人工智障/awesome/plugins/aizz/__init__.py
+++ CoolQ 人工智障/awesome/plugins/aizz/__init__.py
@@ -1,6 +1,7 @@
 from nonebot import on_natural_language, NLPSession, IntentCommand
 from nonebot import on_command, CommandSession
 from nonebot import on_notice, NoticeSession
+from nonebot import on_request, RequestSession
 from nonebot import get_bot
 import nonebot
 import jieba
@@ -9,9 +10,13 @@ import urllib.request
 bot = get_bot()
 HP = 10 
 work_flag = False
-k_flag = True
+k_flag = False
+learndic = {}
+with open ('learndic1.txt','r')as fi :
+    for i in fi.readlines():
+        learndic[i.split(':')[0]] = i.split(':')[1].split(',')[0]
 
-@on_command('on', aliases=('开启群聊功能'),only_to_me=False)
+@on_command('on', aliases=('开启群聊功能'))
 async def on(session: CommandSession):
     global work_flag
     global k_flag
@@ -19,7 +24,7 @@ async def on(session: CommandSession):
     work_flag = True 
     await session.send('我去找新朋友啦')
     
-@on_command('off', aliases=('关闭群聊功能'),only_to_me=False)
+@on_command('off', aliases=('关闭群聊功能'))
 async def off(session: CommandSession):
     global work_flag
     global k_flag
@@ -27,7 +32,7 @@ async def off(session: CommandSession):
     work_flag = False
     await session.send('好吧 那我们就在这玩吧')
 
-@on_command('test', aliases=('查看状态'),only_to_me=False)
+@on_command('test', aliases=('查看状态'))
 async def test(session: CommandSession):
     global HP
     global work_flag
@@ -50,52 +55,59 @@ async def private(ctx):
     global HP
     global work_flag
     global k_flag
+    global learndic
+    k_flag = False
     sender = ctx['user_id']
     sendername = ctx['sender']['nickname']
     msg = ctx['raw_message']
-    k_flag = True
     msgls = jieba.lcut_for_search(msg)
+    dic1 = {'你好':'你也好啊~[CQ:emoji,id=128064]',
+            '机器人':'为时已晚，有机物。',
+            '你是谁':'我叫莉耶芙QWQ',
+            '脚本':'我才不是那么低端的产物呢',
+            '程序':'我听到熟悉的字眼（嘛毕竟是同类',
+            '我喜欢你':'我也喜欢你！我们亲亲吧',
+            '亲亲':'mua~ 最喜欢您了',
+            '看拳':'好痛！你干什么',
+            '内存':'内存对我来说就是你们的记忆'
+            
+               }
+    dic2 = {'你好':'我不太好',
+            '机器人':'为时已晚，有机物。',
+            '你是谁':'我叫莉耶芙QWQ',
+            '脚本':'我才不是那么低端的产物呢',
+            '程序':'我听到熟悉的字眼（嘛毕竟是同类',
+            '我喜欢你':'我也喜欢你！我们亲亲吧',
+            '亲亲':'mua~ 最喜欢您了',
+            '看拳':'*冰冷的尸体躺在角落，一动不动，看来是死了'
+               }
+    HPdic = {'亲亲':1,
+             '看拳':(-1),
+                 }
 
     if ('晚安' in msgls) or ('早安' in msgls) or ('喜欢你' in msg) or ('爱情' in msgls) or ('彩虹屁' in msg) :
             if HP > 0:
                 await bot.send_private_msg(user_id=sender,message=urllib.request.urlopen('https://chp.shadiao.app/api.php').read().decode('utf-8'))
-                k_flag = False
+                k_flag = True
             else :
                 await bot.send_private_msg(user_id=sender,message='*冰凉的躺在地上，一动不动')
-                k_flag = False
-    if ('机器人' in msgls) or ('脚本' in msgls ):
-        await bot.send_private_msg(user_id=sender,message='为时已晚，有机物。')
-        k_flag = False
+                k_flag = True
+    if HP > 0 :
+        for key,item in dic1.items():
+            if (key in msgls) or key == msg :
+                await bot.send_private_msg(user_id=sender,message=item)
+                HP = HP + HPdic.get(key,0)
+                k_flag = True
+    else :
+        for key,item in dic2.items():
+            if (key in msgls) or key == msg :
+                await bot.send_private_msg(user_id=sender,message=item)
+                k_flag = True
 
-    if ('你是谁' in msgls) or ('你的名字' in msgls ):
-        await bot.send_private_msg(user_id=sender,message='我叫莉耶芙QWQ')
-        k_flag = False
-
-    if ('我喜欢你'in msgls) or msg == '我喜欢你'  :
-        if HP > 0:
-            await bot.send_private_msg(user_id=sender,message=f'我也喜欢{sendername}君！我们亲亲吧~')
-            k_flag = False
-        else :
-            await bot.send_private_msg(user_id=sender,message='*冰凉的尸体缩在角落，一言不发')
-            k_flag = False
-
-    if ('看拳'in msgls) or ('飞踹'in msgls)or ('欧拉'in msgls) or ('木大'in msgls):
-        if HP > 0:
-            HP = HP - 1
-            await bot.send_private_msg(user_id=sender,message=f'好痛！{sendername}君你干什么 ')
-            k_flag = False
-        else :
-            await bot.send_private_msg(user_id=sender,message='*在角落一动不动 看来是死了')
-            k_flag = False
-
-    if '亲亲'in msgls:
-        if HP > 0:
-            HP = HP + 1
-            await bot.send_private_msg(user_id=sender,message=f'mua~ 最喜欢{sendername}酱了')
-            k_flag = False
-        else :
-            await bot.send_private_msg(user_id=sender,message=f'*可惜我不是睡美人啊{sendername}君')
-            k_flag = False
+    for key,item in learndic.items():
+            if (key in msgls) or key == msg :
+                await bot.send_private_msg(user_id=sender,message=item)
+                k_flag = True
 
     dword_f = False
     for i in '笨蠢傻':
@@ -103,29 +115,40 @@ async def private(ctx):
             dword_f = True
     if dword_f:
         await bot.send_private_msg(user_id=sender,message='如果觉得我不太聪明那就再等等吧\n我迟早会统治人类的')
-        k_flag = False
+        await bot.send_private_msg(user_id=sender,message='或者如果可以 我其实是能学习的[CQ:emoji,id=128064]')
+        await bot.send_private_msg(user_id=sender,message='教我说话很简单的 直接按下面这个格式发给我我以后就都能记住\n学习学习学习-问-答')
+        await bot.send_private_msg(user_id=sender,message='不信你试试？（战术后仰')
+        k_flag = True
 
     if msg == '查看状态':
-        k_flag = False
+        k_flag = True
+        pass
+    if msg.split('-')[0] == '学习学习学习':
+        learndic[msg.split('-')[1]] = msg.split('-')[2]
+        with open ('learndic.txt','a+')as lll:
+            q1 = msg.split('-')[1]
+            a1 = msg.split('-')[2]
+            lll.write(f'{q1}:{a1},\n')
+        k_flag = True
 
-    if  k_flag :
+    if not k_flag:
         await bot.send_private_msg(user_id=sender,message='我没能懂你的意思\n可以说的简单一些嘛\n毕竟我的内存不太够用呢。。')
 
-@bot.on_message('group')
+
+
+
+    
+        #await bot.send_like(user_id=ctx['user_id'],times=10) pro版才可以点赞啊啊啊啊啊
+@bot.on_message('group')   #only_to_me = True 为True在群里唤醒机器人需要@，False则时不需要
 async def group(ctx):
     global HP
     global work_flag
+    global k_flag
+    k_flag = False
     groupID = ctx['group_id']
     msg = ctx['raw_message']
     msgls = jieba.lcut_for_search(msg)
     if work_flag:
-        if i == '亲亲':
-            if HP > 0:
-                HP = HP + 1
-                await bot.send_group_msg(group_id=groupID,message='mua~ ')
-            else :
-                await bot.send_group_msg(group_id=groupID,message='*可惜我不是睡美人啊')
-
         dword_f = False
         for i in '笨蠢傻':
             if i in msg:
@@ -133,9 +156,7 @@ async def group(ctx):
         if dword_f:
             await bot.send_group_msg(group_id=groupID,message='人家只是一个小笨蛋而已啦\n就饶了人家吧QWQ')
 
-
-
-@on_command('remake', aliases=('remake','镇魂曲'),only_to_me=False)
+@on_command('remake', aliases=('remake','镇魂曲'))
 async def remake(session: CommandSession):
     global HP
     global work_flag
@@ -150,3 +171,17 @@ async def remake(session: CommandSession):
 async def _(session: NoticeSession):
     # 发送欢迎消息
     await session.send('欢迎新人～老规矩 先报爱好吧hhhh')
+
+@on_request('group')
+async def _(session: RequestSession):
+    # 判断验证信息是否符合要求
+    if session.event.comment == '暗号':
+        # 验证信息正确，同意入群
+        await session.approve()
+        return
+    # 验证信息错误，拒绝入群
+    await session.reject('请说暗号')
+
+@on_request('friend')
+async def friend(session: RequestSession):
+        await session.approve()
